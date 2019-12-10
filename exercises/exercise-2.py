8fc1d13eb4a9fcdd44c9f822d2a1ff5653c427c8
doriorio <itsdori@gmail.com>
2019-12-10 12:52:00 -0800

    first commit/usr/local/opt/python/bin/python3.7

--- exercises/exercise-2.py
+++ exercises/exercise-2.py
@@ -2,8 +2,19 @@
 
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
-#      Please enter a word or phrase: 
+
+#Please enter a word or phrase: 
+user_input=input(' Please enter a word or phrase:').lower()
+
 # 2. Print the following message:
+print(f'What you entered is {len(user_input)} characters long')
+
+
+
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
+if (user_input != quit):
+    user_input=input(' Please enter a word or phrase:').lower()
+if (user_input == quit):
+    break
 
