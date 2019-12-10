94ad4e1e9634a61f5c4a5083e031864a0e364a01
doriorio <itsdori@gmail.com>
2019-12-10 13:12:42 -0800

    exercise 4 done

--- exercises/exercise-4.py
+++ exercises/exercise-4.py
@@ -6,9 +6,22 @@
 #      a: 
 #      b:
 #      c:
+tri_status = None
+a_input = input('Enter the lengths of three side of a triangle: a:')
+b_input = input('Enter the lengths of three side of a triangle: b:')
+c_input = input('Enter the lengths of three side of a triangle: c:')
 # 2. Write the code that determines if the triangle is:
 #      equalateral - all three sides are equal in length
-#      scalene - all three sides are unequal in length
+#      scalene - all three sides are unequal in lengthb
 #      isosceles - two sides are the same length
+if (a_input == b_input and b_input == c_input):
+    tri_status = 'equalateral'
+elif (a_input != b_input) and (a_input != c_input) and (b_input != c_input):
+    tri_status = 'scalene'
+else:
+    tri_status = 'isoceles'
+
+
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+print(f'A triangle with sides of {a_input}, {b_input}, {c_input} is a {tri_status} triangle')
