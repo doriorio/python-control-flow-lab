7e098bcb3e14c6467fde934606c7148f0542722c
doriorio <itsdori@gmail.com>
2019-12-10 12:52:33 -0800

    exercise 2 complete

--- exercises/exercise-2.py
+++ exercises/exercise-2.py
@@ -15,6 +15,5 @@ print(f'What you entered is {len(user_input)} characters long')
 # 3. Return to step 1, unless the word 'quit' was entered.
 if (user_input != quit):
     user_input=input(' Please enter a word or phrase:').lower()
-if (user_input == quit):
-    break
+
 
